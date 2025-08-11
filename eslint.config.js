bd60a7f394edacc28dc1d5e7121c4a947801ac1e
akngs <jania902@gmail.com>
2025-08-11 22:39:41 +0900

    chore: restructure eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,24 +5,50 @@ import jsdoc from 'eslint-plugin-jsdoc';
 import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
+  // global ignores
   { ignores: ["dist/**/*", "coverage/**/*"] },
+
+  // eslint
   eslint.configs.recommended,
-  jsdoc.configs['flat/recommended-error'],
-  { ...importPlugin.flatConfigs.recommended, files: ["src/**/*.ts"] },
+
+  // jsdoc
+  {
+    ...jsdoc.configs['flat/recommended-error'],
+    rules: {
+      ...jsdoc.configs['flat/recommended-error'].rules,
+      "jsdoc/require-jsdoc": ["error", { "publicOnly": true }],
+      "jsdoc/require-param-type": "off",
+      "jsdoc/require-returns-type": "off",
+      "jsdoc/require-returns-check": "off",
+    }
+  },
+
+  // import
+  {
+    ...importPlugin.flatConfigs.recommended,
+    files: ["src/**/*.ts"],
+    rules: {
+      ...importPlugin.flatConfigs.recommended.rules,
+      "import/max-dependencies": ["error", {
+        "max": 8,
+        "ignoreTypeImports": false,
+      }],
+    },
+  },
+  
+  // typescript-eslint
   ...tseslint.configs.strictTypeChecked.map(config => ({
     ...config,
     files: ["src/**/*.ts"],
   })),
+
+  // sonarjs
   {
     ...sonarjs.configs.recommended,
     files: ["src/**/*.ts"],
     languageOptions: { parserOptions: { projectService: true } },
     rules: {
       ...sonarjs.configs.recommended.rules,
-      "jsdoc/require-jsdoc": ["error", { "publicOnly": true }],
-      "jsdoc/require-param-type": "off",
-      "jsdoc/require-returns-type": "off",
-      "jsdoc/require-returns-check": "off",
       "sonarjs/todo-tag": "off",
       "sonarjs/pseudo-random": "off",
       "sonarjs/no-os-command-from-path": "off",
@@ -36,15 +62,13 @@ export default tseslint.config(
       "sonarjs/slow-regex": "off",
       "no-useless-escape": "off",
       "no-magic-numbers": "off",
-      "import/max-dependencies": ["error", {
-        "max": 8,
-        "ignoreTypeImports": false,
-      }],
       "@typescript-eslint/restrict-template-expressions": "off",
       "@typescript-eslint/switch-exhaustiveness-check": "error",
       "@typescript-eslint/no-magic-numbers": ["error", {ignore: [-2, -1, 0, 1, 2, 10, 42, 100], ignoreEnums: true, ignoreNumericLiteralTypes: true, ignoreReadonlyClassProperties: true, ignoreTypeIndexes: true}],
     }
   },
+
+  // sonarjs for tests
   {
     files: ["src/**/*.test.ts"],
     rules: {
